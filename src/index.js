4f941c61863af2c4686b0f5ea2cbeccd9c7a58e6
dannyb21892 <dannyb21892@verizon.net>
2018-04-11 09:54:26 -0400

    discussion qs

--- src/index.js
+++ src/index.js
@@ -3,4 +3,40 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+
+let pic = document.getElementById("profile_picture")
+let name = document.getElementById("fullname")
+let email = document.getElementById("email")
+let street = document.getElementById("street")
+let city = document.getElementById("city")
+let state = document.getElementById("state")
+let postcode = document.getElementById("postcode")
+let phone = document.getElementById("phone")
+let cell = document.getElementById("cell")
+let dob = document.getElementById("date_of_birth")
+
+let button = document.getElementsByClassName("btn btn-primary")[0]
+button.addEventListener("click", populatePage)
+
+function populatePage(){
+  fetch("https://randomuser.me/api/")
+  .then(function(res) {return res.json()})
+  .then(renderPage)
+
+  function renderPage(data){
+    data = data.results[0]
+    name.innerText = ((data.name.title + ". " + data.name.first + " " + data.name.last))
+    email.innerText = data.email
+    street.innerText = data.location.street
+    city.innerText = data.location.city
+    state.innerText = data.location.state
+    postcode.innerText = data.location.postcode
+    phone.innerText = data.phone
+    cell.innerText = data.cell
+    dob.innerText = data.dob
+    pic.src = data.picture.large
+
+
+  }
+}
 });
