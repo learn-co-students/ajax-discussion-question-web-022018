15da0ca851cbca6dc9e920e13090e77c9569f6e0
github username <github email address>
2018-04-11 10:49:34 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,22 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+
+  let pic = document.getElementById("profile_picture")
+  let name = document.getElementById("fullname")
+  let button = document.getElementsClassName("btn btn-primary")[0]
+
+  button.addEventListener('click', populatePage)
+
+  function populatePage(){
+    fetch('https://randomuser.me/api/')
+    .then( res => res.json() )
+    .then(renderPage)
+
+    function renderPage(){
+      data = data.results[0]
+      name.innerText = ((data.name.title + ". " + data.name.first + " " + data.name.last))
+    }
+  }
+  
 });
